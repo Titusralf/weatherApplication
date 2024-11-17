a261fb91f92c59e44edba3a10d975f716eb5cb9f
Titusralf <158459153+Titusralf@users.noreply.github.com>
2024-11-17 17:50:05 +0200

    Added real live data using API for 5 days icons, min and max temperature

--- index.js
+++ index.js
@@ -59,6 +59,8 @@ function displayTemperature(response) {
 
     iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="current-temperature-icon" />`;
 
+    getForecast(response.data.city);
+
     // Convert the timestamp to a Date object
     let date = new Date(response.data.time * 1000);
     timeElement.innerHTML = formatDate(date);
@@ -80,3 +82,49 @@ function fetchTemperature(city) {
 
   axios.get(apiUrl).then(displayTemperature);
 }
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "3fe65bdtbe2f46e90e4839f06a35o867";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  console.log(response.data);
+
+  let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+          <div class="weather-forecast-day">
+            <div class="weather-forecast-date">${formatDay(day.time)}</div>
+            
+            <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
+          
+            <div class="weather-forecast-temperatures">
+              <div class="weather-forecast-temperature">
+                <strong>${Math.round(day.temperature.maximum)}°</strong>
+              </div>
+              <div class="weather-forecast-temperature">${Math.round(
+                day.temperature.minimum
+              )}°</div>
+            </div>
+          </div>
+          `;
+    }
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+fetchTemperature("Centurion");
